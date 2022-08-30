702da21f7b1026dd0b8cfb379bad035ae3c7be8f
Aleksey <dieselryder@mail.ru>
2022-08-30 11:26:26 +0300

    Задача 8

--- Задача 8/Program.cs
+++ Задача 8/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Задача 8. Вывод четных чисел в диапазоне от 1 до N");
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+int i = 1;
+string temp = "";
+while (i <= n)
+{
+    if (i % 2 == 0)
+    {
+        if (temp == "")
+        {
+            temp = temp + i;
+        }
+        else
+        {
+            temp = temp + ", " + i;
+        }
+    }
+    i++;
+}
+Console.Write("Четные числа: {0}", temp);
